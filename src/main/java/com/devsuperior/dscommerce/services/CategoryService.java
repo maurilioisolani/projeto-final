c8c396cd9fe603387b4c57e4010c44fad21097cc
maurilioisolani <maurilioisolani@gmail.com>
2025-04-20 15:15:53 -0300

    Criação do endpoin de category

---
+++ src/main/java/com/devsuperior/dscommerce/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.CategorieDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CategorieDTO> findAll() {
+        List<Category> result = repository.findAll();
+        return result.stream().map(CategorieDTO::new).toList();
+    }
+
+}
