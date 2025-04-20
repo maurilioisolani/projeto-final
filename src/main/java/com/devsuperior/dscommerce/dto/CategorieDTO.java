e9bd200ef5d5cf7344ac4a692a0deeb69149e282
maurilioisolani <maurilioisolani@gmail.com>
2025-04-20 09:42:57 -0300

    Criação do CategorieDTO

---
+++ src/main/java/com/devsuperior/dscommerce/dto/CategorieDTO.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Category;
+
+public class CategorieDTO {
+
+    private Long id;
+    private String name;
+
+    public CategorieDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategorieDTO(Category entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+}
